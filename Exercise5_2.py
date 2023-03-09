6090cd3af501a967725937e5cd51653617eac9d2
Khavushka <khavushka@gmail.com>
2023-03-09 14:23:22 +0100

    Final answer

--- Exercise5_2.py
+++ Exercise5_2.py
@@ -1,48 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Define the data set
-data = np.array([[1.5, 4], [6.6, 8.3], [6.6, 4]])
+# Definere dataset
+X = np.array([[10,1],[2,3], [3,4], [1,5], [7,7], [6,8], [7,8], [7,9]])
 
-# Define the initial centroids
-centroids = np.array([[1.5, 4], [6.6, 8.3], [6.6, 4]])
+# Numer af clusters
+k_values = [2, 3, 4, 5]
 
-# Define the number of iterations
-num_iters = 10
+for k in k_values:
+    # Random centroids 
+    centroids = np.array([[6.7,4], [1.5,4], [6.7,8.3]])
+    
+    # Fortsat indtil convergence
+    for i in range(100):
+        # Assign each data point to the closest centroid
+        distances = np.sqrt(((X - centroids[:, np.newaxis])**2).sum(axis=2))
+        labels = np.argmin(distances, axis=0)
 
-# Define the number of clusters
-k = 2
-
-# Define the least squares assignment function
-def least_squares_assignment(data, centroids):
-    distances = np.sqrt(((data - centroids[:, np.newaxis])**2).sum(axis=2))
-    return np.argmin(distances, axis=0)
-
-# Initialize a list to store the TD2 values for each iteration
-td2_values = []
-
-# Perform k-means algorithm for num_iters iterations
-for i in range(num_iters):
-    # Assign each data point to the closest centroid
-    assignments = least_squares_assignment(data, centroids)
-
-    # Update the centroids as the mean of the data points in each cluster
-    for j in range(k):
-        centroids[j] = np.mean(data[assignments == j], axis=0)
-
-    # Compute the TD2 value
-    td2 = np.sum((data - centroids[assignments])**2)
-    td2_values.append(td2)
-
-    # Plot the current state of the clustering
-    colors = ['r', 'b', 'g']
-    plt.figure()
-    for j in range(k):
-        plt.scatter(data[assignments == j, 0], data[assignments == j, 1], c=colors[j], label='Cluster {}'.format(j+1))
-    plt.scatter(centroids[:, 0], centroids[:, 1], c='k', marker='x', s=100, label='Centroids')
-    plt.legend()
-    plt.title('Iteration {}'.format(i+1))
-    # plt.show()
-
-# Print the final TD2 value
-print('Final TD2 value:', td2_values[-1])
+        # Update centroids
+        for j in range(k):
+            centroids[j] = X[labels == j].mean(axis=0)
+    
+    # Compute TD2 
+    td2 = ((X - centroids[labels])**2).sum() / X.shape[0]
+    print(f'For k={k}, TD2={td2:.2f}')
+    # print('Final TD2 value: ', td2)
