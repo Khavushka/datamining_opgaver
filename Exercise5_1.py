eea3438c4169cf2be763cb8afb8543757809eb61
Khavushka <khavushka@gmail.com>
2023-03-10 14:40:15 +0100

    Opgave 2 med td2

--- Exercise5_1.py
+++ Exercise5_1.py
@@ -5,7 +5,6 @@
 import numpy as np
 from sklearn.metrics import silhouette_score
 
-
 # Define data points and corresponding labels
 X = np.array([[10,1],[2,3], [3,4], [1,5], [7,7], [6,8], [7,8], [7,9]])
 labels1 = np.array([1, 1, 1, 1, 0, 0, 0, 0])
@@ -35,10 +34,15 @@ for sil in [labels1, labels2, labels3]:
     silhouette = []
     for i in sil:
         silhouette = silhouette_score(X, sil)
-    print("The silhouette:", silhouette)
+    print("The silhouette value for labels:", silhouette)
+
 '''
 Svar:
 The TD^2 value for labels [1 1 1 1 0 0 0 0] is: 61.5
 The TD^2 value for labels [1 0 0 0 0 0 0 0] is: 72.85714285714285
 The TD^2 value for labels [0 1 1 1 0 0 0 0] is: 54.400000000000006
+The silhouette value for labels: 0.5161518748019878
+The silhouette value for labels: 0.41330904778417
+The silhouette value for labels: 0.5467209394773982
+
 '''
--- Exercise5_2.py
+++ Exercise5_2.py
@@ -6,20 +6,24 @@ import matplotlib.pyplot as plt
 
 # Definere dataset
 X = np.array([[10,1],[2,3], [3,4], [1,5], [7,7], [6,8], [7,8], [7,9]])
+labels1 = np.array([1, 1, 1, 1, 0, 0, 0, 0])
+labels2 = np.array([1, 0, 0, 0, 0, 0, 0, 0])
+labels3 = np.array([0, 1, 1, 1, 0, 0, 0, 0])
 
-# Numer af clusters
+# Nummer af clusters
 k_values = [2, 3, 4, 5]
+# k_values = [3]
 
 for k in k_values:
     # Random centroids 
-    centroids = np.array([[6.7,4], [1.5,4], [6.7,8.3]])
-    
+    centroids = np.array([[6.7,4], [1.5,4], [6.7,8.3], [2,2],[4,3]])
+    # centroids = np.random.rand(k, 2) * 10
+
     # Fortsat indtil convergence
     for i in range(100):
         # Assign each data point to the closest centroid
         distances = np.sqrt(((X - centroids[:, np.newaxis])**2).sum(axis=2))
         labels = np.argmin(distances, axis=0)
-        # print(X)
 
         # Update centroids
         for j in range(k):
@@ -29,4 +33,3 @@ for k in k_values:
     # Compute TD2 
     td2 = ((X - centroids[labels])**2).sum() / X.shape[0]
     print(f'For k={k}, TD2={td2:.2f}')
-    # print('Final TD2 value: ', td2)
