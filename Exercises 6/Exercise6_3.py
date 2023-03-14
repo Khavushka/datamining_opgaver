e99cb370d68341e4b3a0285327c94166b79a5bcd
Khavushka <khavushka@gmail.com>
2023-03-14 20:31:54 +0100

    Tildel testpunktet til majoritetsklassen blandt de k nærmeste noaboer

--- Exercises 6/Exercise6_3.py
+++ Exercises 6/Exercise6_3.py
@@ -5,17 +5,19 @@ from collections import Counter
 # Define the data points and their labels
 X = np.array([[8,2],[8,3],[7,2],[7,4],[7,6],[6,1],[6,3],[6,8],[5,4],[5,7],[4,6],[1,3],[1,8],[1,9]])
 y = ['rectangle', 'rectangle', 'rectangle', 'rectangle', 'circle', 'rectangle', 'rectangle', 'circle', 'rectangle', 'circle', 'circle', 'circle', 'circle', 'circle']
+# y = np.array([0,0,0,0,0,1,1,0,1,1,1,1,1,1])
 
 # Define the test point
 test_point = np.array([6, 6])
 
 # Define the value of k
 # k = 4
-# k = 7
-k = 10
+k = 7
+# k = 10
 
 # Beregn Manhattan-afstandene mellem testpunktet og alle datapunkter
 distances = [cityblock(test_point, x) for x in X]
+print(distances)
 
 # Få indeksene for de k nærmeste naboer
 knn_indices = np.argsort(distances)[:k]
