647b57b28861c966cbcdad91b9ad8cb1b001e5b2
Khavushka <khavushka@gmail.com>
2023-04-21 11:57:06 +0200

    scatter points on the main axes

--- Exercise 11/Exercise11_2.py
+++ Exercise 11/Exercise11_2.py
@@ -1,3 +1,50 @@
+# import numpy as np
+# import pandas as pd
+# import matplotlib.pyplot as plt
+# from sklearn.cluster import KMeans, DBSCAN
+# from sklearn.mixture import GaussianMixture
+# from sklearn.preprocessing import StandardScaler
+# from sklearn.metrics import silhouette_score
+
+# # Load Wine dataset
+# url = "http://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data"
+# wine_df = pd.read_csv(url, header=None)
+# X = wine_df.iloc[:, 1:].values
+
+# # Preprocess data
+# scaler = StandardScaler()
+# X_scaled = scaler.fit_transform(X)
+
+# # Set parameters for clustering algorithms
+# kmeans = KMeans(n_clusters=3, random_state=42)
+# dbscan = DBSCAN(eps=1, min_samples=5)
+# gmm = GaussianMixture(n_components=3, random_state=42)
+
+# # Fit clustering algorithms
+# kmeans.fit(X_scaled)
+# dbscan.fit(X_scaled)
+# gmm.fit(X_scaled)
+
+# # Get cluster labels
+# kmeans_labels = kmeans.labels_
+# dbscan_labels = dbscan.labels_
+# gmm_labels = gmm.predict(X_scaled)
+
+# # Calculate Silhouette scores
+# kmeans_score = silhouette_score(X_scaled, kmeans_labels)
+# dbscan_score = silhouette_score(X_scaled, dbscan_labels) if -1 not in dbscan_labels else np.nan
+# gmm_score = silhouette_score(X_scaled, gmm_labels)
+
+# # Plot results
+# fig, ax = plt.subplots(1, 3, figsize=(12, 4))
+# ax[0].scatter(X_scaled[:, 0], X_scaled[:, 1], c=kmeans_labels)
+# ax[0].set_title(f"KMeans\nSilhouette score: {kmeans_score:.2f}")
+# ax[1].scatter(X_scaled[:, 0], X_scaled[:, 1], c=dbscan_labels)
+# ax[1].set_title(f"DBSCAN\nSilhouette score: {dbscan_score:.2f}")
+# ax[2].scatter(X_scaled[:, 0], X_scaled[:, 1], c=gmm_labels)
+# ax[2].set_title(f"EM-Clustering\nSilhouette score: {gmm_score:.2f}")
+# plt.show()
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -15,7 +62,7 @@ X_scaled = scaler.fit_transform(X)
 
 # Parameters for clustering algorithms
 kmeans = KMeans(n_clusters=3, random_state=42)
-dbscan = DBSCAN(eps=1, min_samples=5)
+dbscan = DBSCAN(eps=2, min_samples=3)
 gmm = GaussianMixture(n_components=3, random_state=42)
 
 kmeans.fit(X_scaled)
@@ -41,3 +88,7 @@ ax[1].set_title(f"DBSCAN\nSilhouette score: {dbscan_score:.2f}")
 ax[2].scatter(X_scaled[:, 0], X_scaled[:, 1], c=gmm_labels)
 ax[2].set_title(f"EM-Clustering\nSilhouette score: {gmm_score:.2f}")
 plt.show()
+
+print("Silhouette-score for K-means: ", kmeans_score)
+print("Silhouette-score for DBSCAN: ", dbscan_score)
+print("Silhouette-score for EM-clustering: ", gmm_score)
--- Exercise 11/Silh_score2.py
+++ Exercise 11/Silh_score2.py
@@ -1,49 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.cluster import KMeans, DBSCAN
-from sklearn.mixture import GaussianMixture
-from sklearn.metrics import silhouette_score
-
-# Load the spiral dataset
-url = "https://cs.joensuu.fi/sipu/datasets/spiral.txt"
-data = np.loadtxt(url)
-
-# K-means clustering
-kmeans = KMeans(n_clusters=3, random_state=0)
-kmeans_labels = kmeans.fit_predict(data)
-kmeans_score = silhouette_score(data, kmeans_labels)
-
-# DBSCAN clustering
-dbscan = DBSCAN(eps=2, min_samples=3)
-dbscan_labels = dbscan.fit_predict(data)
-dbscan_score = silhouette_score(data, dbscan_labels)
-
-# EM-clustering
-em = GaussianMixture(n_components=3, random_state=0)
-em_labels = em.fit_predict(data)
-em_score = silhouette_score(data, em_labels)
-
-# Print silhouette coefficients for hver clustering solution
-print(f"K-means score: {kmeans_score:.3f}")
-print(f"DBSCAN score: {dbscan_score:.3f}")
-print(f"EM-clustering score: {em_score:.3f}")
-
-# Plot the clustering results
-fig, ax = plt.subplots(1, 3, figsize=(12, 4))
-
-ax[0].scatter(data[:, 0], data[:, 1], c=kmeans_labels)
-ax[0].set_title(f"K-means (score={kmeans_score:.3f})")
-
-ax[1].scatter(data[:, 0], data[:, 1], c=dbscan_labels)
-ax[1].set_title(f"DBSCAN (score={dbscan_score:.3f})")
-
-ax[2].scatter(data[:, 0], data[:, 1], c=em_labels)
-ax[2].set_title(f"EM-clustering (score={em_score:.3f})")
-
-plt.show()
-
-'''Silhouette-score til second-dataset
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans, DBSCAN
@@ -92,5 +47,3 @@ print()
 
 print("EM Clustering")
 print("Silhouette Coefficient:", gmm_silhouette)
-
-'''
