a641b955a15769dcc8b07b22fd1ca70a607a661d
Khavushka <khavushka@gmail.com>
2023-04-21 09:49:33 +0200

    Last commit

--- Exercise 11/Exercise11_2.py
+++ Exercise 11/Exercise11_2.py
@@ -1,115 +1,90 @@
-# 1-solution ------------------
-# import pandas as pd
 # import numpy as np
+# import pandas as pd
 # import matplotlib.pyplot as plt
 # from sklearn.cluster import KMeans, DBSCAN
 # from sklearn.mixture import GaussianMixture
+# from sklearn.preprocessing import StandardScaler
+# from sklearn.metrics import silhouette_score
 
-# # Load the wine dataset
-# url = "https://cs.joensuu.fi/sipu/datasets/wine.txt"
-# data = pd.read_csv(url, delimiter='\t', header=None, names=['x', 'y', 'z', 'label'])
-# X = data.drop('label', axis=1)
+# # Load Wine dataset
+# url = "http://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data"
+# wine_df = pd.read_csv(url, header=None)
+# X = wine_df.iloc[:, 1:].values
 
-# # Visualize the dataset
-# fig = plt.figure(figsize=(8, 6))
-# ax = fig.add_subplot(projection='3d')
-# ax.scatter(X['x'], X['y'], X['z'], c=data['label'], cmap='viridis')
-# ax.set_xlabel('x')
-# ax.set_ylabel('y')
-# ax.set_zlabel('z')
-# plt.show()
+# # Preprocess data
+# scaler = StandardScaler()
+# X_scaled = scaler.fit_transform(X)
 
-# # Apply K-means clustering
+# # Set parameters for clustering algorithms
 # kmeans = KMeans(n_clusters=3, random_state=42)
-# kmeans_labels = kmeans.fit_predict(X)
+# dbscan = DBSCAN(eps=1, min_samples=5)
+# gmm = GaussianMixture(n_components=3, random_state=42)
 
-# # Apply DBSCAN clustering
-# dbscan = DBSCAN(eps=0.5, min_samples=5)
-# dbscan_labels = dbscan.fit_predict(X)
+# # Fit clustering algorithms
+# kmeans.fit(X_scaled)
+# dbscan.fit(X_scaled)
+# gmm.fit(X_scaled)
 
-# # Apply EM-clustering
-# em = GaussianMixture(n_components=3, random_state=42)
-# em_labels = em.fit_predict(X)
+# # Get cluster labels
+# kmeans_labels = kmeans.labels_
+# dbscan_labels = dbscan.labels_
+# gmm_labels = gmm.predict(X_scaled)
 
-# # Visualize the clustering results
-# fig = plt.figure(figsize=(16, 6))
+# # Calculate Silhouette scores
+# kmeans_score = silhouette_score(X_scaled, kmeans_labels)
+# dbscan_score = silhouette_score(X_scaled, dbscan_labels) if -1 not in dbscan_labels else np.nan
+# gmm_score = silhouette_score(X_scaled, gmm_labels)
 
-# ax1 = fig.add_subplot(131, projection='3d')
-# ax1.scatter(X['x'], X['y'], X['z'], c=kmeans_labels, cmap='viridis')
-# ax1.set_xlabel('x')
-# ax1.set_ylabel('y')
-# ax1.set_zlabel('z')
-# ax1.set_title('K-means')
-
-# ax2 = fig.add_subplot(132, projection='3d')
-# ax2.scatter(X['x'], X['y'], X['z'], c=dbscan_labels, cmap='viridis')
-# ax2.set_xlabel('x')
-# ax2.set_ylabel('y')
-# ax2.set_zlabel('z')
-# ax2.set_title('DBSCAN')
-
-# ax3 = fig.add_subplot(133, projection='3d')
-# ax3.scatter(X['x'], X['y'], X['z'], c=em_labels, cmap='viridis')
-# ax3.set_xlabel('x')
-# ax3.set_ylabel('y')
-# ax3.set_zlabel('z')
-# ax3.set_title('EM-clustering')
-
+# # Plot results
+# fig, ax = plt.subplots(1, 3, figsize=(12, 4))
+# ax[0].scatter(X_scaled[:, 0], X_scaled[:, 1], c=kmeans_labels)
+# ax[0].set_title(f"KMeans\nSilhouette score: {kmeans_score:.2f}")
+# ax[1].scatter(X_scaled[:, 0], X_scaled[:, 1], c=dbscan_labels)
+# ax[1].set_title(f"DBSCAN\nSilhouette score: {dbscan_score:.2f}")
+# ax[2].scatter(X_scaled[:, 0], X_scaled[:, 1], c=gmm_labels)
+# ax[2].set_title(f"EM-Clustering\nSilhouette score: {gmm_score:.2f}")
 # plt.show()
 
-'''BASIS 3D - code
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans, DBSCAN
 from sklearn.mixture import GaussianMixture
-from mpl_toolkits.mplot3d import Axes3D
-import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import silhouette_score
 
-# Load data
-url_data = pd.read_csv('urls.csv')
-url_data = url_data.dropna()
-url_data = url_data[['url_length', 'num_special_chars', 'num_numbers']]
+# Load Wine dataset
+url = "http://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data"
+wine_df = pd.read_csv(url, header=None)
+X = wine_df.iloc[:, 1:].values
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
 
-# Scale data
-url_data = (url_data - url_data.mean()) / url_data.std()
+# Parameters for clustering algorithms
+kmeans = KMeans(n_clusters=3, random_state=42)
+dbscan = DBSCAN(eps=1, min_samples=5)
+gmm = GaussianMixture(n_components=3, random_state=42)
 
-# K-means clustering
-kmeans = KMeans(n_clusters=3, random_state=0).fit(url_data)
-url_data['kmeans_cluster'] = kmeans.labels_
+kmeans.fit(X_scaled)
+dbscan.fit(X_scaled)
+gmm.fit(X_scaled)
 
-# DBSCAN clustering
-dbscan = DBSCAN(eps=0.3, min_samples=5).fit(url_data)
-url_data['dbscan_cluster'] = dbscan.labels_
+# Get cluster labels
+kmeans_labels = kmeans.labels_
+dbscan_labels = dbscan.labels_
+gmm_labels = gmm.predict(X_scaled)
 
-# EM-clustering
-gmm = GaussianMixture(n_components=3, random_state=0).fit(url_data)
-url_data['gmm_cluster'] = gmm.predict(url_data)
+# Beregner Silhouette scores
+kmeans_score = silhouette_score(X_scaled, kmeans_labels)
+dbscan_score = silhouette_score(X_scaled, dbscan_labels) if -1 not in dbscan_labels else np.nan
+gmm_score = silhouette_score(X_scaled, gmm_labels)
 
-# Plot clusters
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.scatter(url_data['url_length'], url_data['num_special_chars'], url_data['num_numbers'], c=url_data['kmeans_cluster'])
-ax.set_title('K-means Clustering')
-ax.set_xlabel('URL Length')
-ax.set_ylabel('Number of Special Characters')
-ax.set_zlabel('Number of Numbers')
+# Plot results
+fig, ax = plt.subplots(1, 3, figsize=(12, 4))
+ax[0].scatter(X_scaled[:, 0], X_scaled[:, 1], c=kmeans_labels)
+ax[0].set_title(f"KMeans\nSilhouette score: {kmeans_score:.2f}")
+ax[1].scatter(X_scaled[:, 0], X_scaled[:, 1], c=dbscan_labels)
+ax[1].set_title(f"DBSCAN\nSilhouette score: {dbscan_score:.2f}")
+ax[2].scatter(X_scaled[:, 0], X_scaled[:, 1], c=gmm_labels)
+ax[2].set_title(f"EM-Clustering\nSilhouette score: {gmm_score:.2f}")
 plt.show()
-
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.scatter(url_data['url_length'], url_data['num_special_chars'], url_data['num_numbers'], c=url_data['dbscan_cluster'])
-ax.set_title('DBSCAN Clustering')
-ax.set_xlabel('URL Length')
-ax.set_ylabel('Number of Special Characters')
-ax.set_zlabel('Number of Numbers')
-plt.show()
-
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.scatter(url_data['url_length'], url_data['num_special_chars'], url_data['num_numbers'], c=url_data['gmm_cluster'])
-ax.set_title('EM-Clustering')
-ax.set_xlabel('URL Length')
-ax.set_ylabel('Number of Special Characters')
-ax.set_zlabel('Number of Numbers')
-plt.show()
-'''
