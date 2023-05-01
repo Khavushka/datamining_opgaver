60e4429f71e5926e8cee5f3e082a254484602567
Khavushka <khavushka@gmail.com>
2023-05-01 11:36:41 +0200

    Download dataset Iris

--- Exercise 12/Exercise12_3.py
+++ Exercise 12/Exercise12_3.py
@@ -11,6 +11,7 @@ from sklearn.metrics import accuracy_score
 iris = load_iris()
 X = iris.data
 y = iris.target
+print(iris)
 
 # Split dataset into training
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
