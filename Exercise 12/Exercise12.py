0c7d4ac7a0d0bb29c3a187eb3c1ad6090ddc7f44
Khavushka <khavushka@gmail.com>
2023-05-01 06:45:11 +0200

    Using sklearn

--- Exercise 12/Exercise12.py
+++ Exercise 12/Exercise12.py
@@ -1,8 +1,19 @@
-import matplotlib.pyplot as plt
-from sklearn.tree import plot_tree
+# import matplotlib.pyplot as plt
+# from sklearn.tree import plot_tree
 
-plot_tree(clf, 
-          feature_names=iris.get('feature_names'), 
-          class_names=iris.get('target_names'))
+# plot_tree(clf, 
+#           feature_names=iris.get('feature_names'), 
+#           class_names=iris.get('target_names'))
 
-plt.show()
+# plt.show()
+
+
+import pandas as pd
+from sklearn.tree import DeciosionTreeClassifier, export_text
+
+# dataset
+df = pd.DataFrame({
+    
+})
+
+df_encoded = 
