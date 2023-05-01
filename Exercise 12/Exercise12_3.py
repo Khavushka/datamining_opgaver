48bed960fd00f9b97dded2d060548ec51285d2e1
Khavushka <khavushka@gmail.com>
2023-05-01 11:25:50 +0200

    Der kommer lidt mere kode senere

--- Exercise 12/Exercise12_3.py
+++ Exercise 12/Exercise12_3.py
@@ -14,24 +14,3 @@ y = iris.target
 
 # Split dataset into training
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
-
-# Decision Tree Classifier
-tree_clf = DecisionTreeClassifier(random_state=42)
-tree_clf.fit(X_train, y_train)
-tree_pred = tree_clf.predict(X_test)
-tree_acc = accuracy_score(y_test, tree_pred)
-print("Decision Tree Accuracy:", tree_acc)
-
-# Naive Bayes Classifier
-nb_clf = GaussianNB()
-nb_clf.fit(X_train, y_train)
-nb_pred = nb_clf.predict(X_test)
-nb_acc = accuracy_score(y_test, nb_pred)
-print("Naive Bayes Accuracy:", nb_acc)
-
-# k-NN Classifier
-knn_clf = KNeighborsClassifier(n_neighbors=3)
-knn_clf.fit(X_train, y_train)
-knn_pred = knn_clf.predict(X_test)
-knn_acc = accuracy_score(y_test, knn_pred)
-print("k-NN Accuracy:", knn_acc)
