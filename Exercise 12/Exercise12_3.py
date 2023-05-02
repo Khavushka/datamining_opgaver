8b8c6e591ef73556b05129ca7425af4d2b7e3443
Khavushka <khavushka@gmail.com>
2023-05-02 09:02:23 +0200

    Ændringer

--- Exercise 12/Exercise12_3.py
+++ Exercise 12/Exercise12_3.py
@@ -1,4 +1,5 @@
 # Decision trees, naive Bayes, and k-nn classification
+# https://www.simplilearn.com/tutorials/machine-learning-tutorial/decision-tree-in-python
 import numpy as np
 from sklearn.datasets import load_iris
 from sklearn.tree import DecisionTreeClassifier
@@ -14,4 +15,22 @@ y = iris.target
 print(iris)
 
 # Split dataset into training
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=100)
+
+# Decision Tree Classifier
+# tree_klas = DecisionTreeClassifier(random_state=42)
+# tree_klas.fit(X_train, y_train)
+# print(tree_klas)
+
+clf_entropy = DecisionTreeClassifier(
+    criterion = "entropy",
+    random_state = 100,
+    max_depth=3,
+    min_samples_leaf=5,
+)
+
+clf_entropy.fit(X_train, y_train)
+
+# Naive Bayes Classifier
+
+# k-NN Classifier
