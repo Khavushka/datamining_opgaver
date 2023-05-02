b11b2c7cd73d1bb41f4ebcb67795119012efc5ff
Khavushka <khavushka@gmail.com>
2023-05-02 09:04:30 +0200

    Done

--- Exercise 12/Exercise12_3.py
+++ Exercise 12/Exercise12_3.py
@@ -18,19 +18,32 @@ print(iris)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=100)
 
 # Decision Tree Classifier
-# tree_klas = DecisionTreeClassifier(random_state=42)
-# tree_klas.fit(X_train, y_train)
-# print(tree_klas)
+tree_clf = DecisionTreeClassifier(random_state=42)
+tree_clf.fit(X_train, y_train)
+tree_pred = tree_clf.predict(X_test)
+tree_acc = accuracy_score(y_test, tree_pred)
+print("Decision Tree Accuracy:", tree_acc)
 
+# Naive Bayes Classifier
+nb_clf = GaussianNB()
+nb_clf.fit(X_train, y_train)
+nb_pred = nb_clf.predict(X_test)
+nb_acc = accuracy_score(y_test, nb_pred)
+print("Naive Bayes Accuracy:", nb_acc)
+
+# k-NN Classifier
+knn_clf = KNeighborsClassifier(n_neighbors=3)
+knn_clf.fit(X_train, y_train)
+knn_pred = knn_clf.predict(X_test)
+knn_acc = accuracy_score(y_test, knn_pred)
+print("k-NN Accuracy:", knn_acc)
+
+
+# Der her er ikke noget til opgaven, mere til at afprøve noget ting
 clf_entropy = DecisionTreeClassifier(
     criterion = "entropy",
     random_state = 100,
     max_depth=3,
     min_samples_leaf=5,
 )
-
 clf_entropy.fit(X_train, y_train)
-
-# Naive Bayes Classifier
-
-# k-NN Classifier
