2137b23c41849743c3b5da70bde362ac0b552378
Khavushka <khavushka@gmail.com>
2023-05-03 14:09:40 +0200

    Initial commit

--- Exercise 12/k_NN.py
+++ Exercise 12/k_NN.py
@@ -19,4 +19,104 @@ iris = datasets.load_iris()
 # np.c_ is the numpy concatenate function
 iris_df = pd.DataFrame(data= np.c_[iris['data'], iris['target']],
                       columns= iris['feature_names'] + ['target'])
-iris_df.head()
+
+x= iris_df.iloc[:, :-1]
+y= iris_df.iloc[:, -1]
+
+
+# split the data into train and test sets
+x_train, x_test, y_train, y_test= train_test_split(x, y,
+                                                   test_size= 0.2,
+                                                   shuffle= True, #shuffle the data to avoid bias
+                                                   random_state= 0)
+x_train= np.asarray(x_train)
+y_train= np.asarray(y_train)
+
+x_test= np.asarray(x_test)
+y_test= np.asarray(y_test)
+
+
+print(f'training set size: {x_train.shape[0]} samples \ntest set size: {x_test.shape[0]} samples')
+
+scaler= Normalizer().fit(x_train) # the scaler is fitted to the training set
+normalized_x_train= scaler.transform(x_train) # the scaler is applied to the training set
+normalized_x_test= scaler.transform(x_test) # the scaler is applied to the test set
+
+
+print('x train before Normalization')
+print(x_train[0:5])
+print('\nx train after Normalization')
+print(normalized_x_train[0:5])
+
+# kNN (Eucledean distance)
+def distance_ecu(x_train, x_test_point):
+  """
+  Input:
+    - x_train: corresponding to the training data
+    - x_test_point: corresponding to the test point
+
+  Output:
+    -distances: The distances between the test point and each point in the training data.
+
+  """
+  distances= []  ## create empty list called distances
+  for row in range(len(x_train)): ## Loop over the rows of x_train
+      current_train_point= x_train[row] #Get them point by point
+      current_distance= 0 ## initialize the distance by zero
+
+      for col in range(len(current_train_point)): ## Loop over the columns of the row
+          
+          current_distance += (current_train_point[col] - x_test_point[col]) **2
+          ## Or current_distance = current_distance + (x_train[i] - x_test_point[i])**2
+      current_distance= np.sqrt(current_distance)
+
+      distances.append(current_distance) ## Append the distances
+
+  # Store distances in a dataframe
+  distances= pd.DataFrame(data=distances,columns=['dist'])
+  return distances
+
+# kNN (nearest neighbour)
+def nearest_neighbors(distance_point, K):
+    # Sort values using the sort_values function
+    df_nearest= distance_point.sort_values(by=['dist'], axis=0)
+
+    ## Take only the first K neighbors
+    df_nearest= df_nearest[:K]
+    return df_nearest
+
+# kNN (majority vote)
+def voting(df_nearest, y_train):
+    ## Use the Counter Object to get the labels with K nearest neighbors.
+    counter_vote= Counter(y_train[df_nearest.index])
+
+    y_pred= counter_vote.most_common()[0][0]   # Majority Voting
+
+    return y_pred
+
+# kNN algorithm: everything together
+def KNN_from_scratch(x_train, y_train, x_test, K):
+    y_pred=[]
+
+    ## Loop over all the test set and perform the three steps
+    for x_test_point in x_test:
+      distance_point  = distance_ecu(x_train, x_test_point)  ## Step 1
+      df_nearest_point= nearest_neighbors(distance_point, K)  ## Step 2
+      y_pred_point    = voting(df_nearest_point, y_train) ## Step 3
+      y_pred.append(y_pred_point)
+
+    return y_pred  
+  
+K=3
+y_pred_scratch= KNN_from_scratch(normalized_x_train, y_train, normalized_x_test, K)
+print(y_pred_scratch)
+
+# implementation with Sklearn 
+knn=KNeighborsClassifier(K)
+knn.fit(normalized_x_train, y_train)
+y_pred_sklearn= knn.predict(normalized_x_test)
+print(y_pred_sklearn)
+
+# Accuracy of both methods
+print(f'The accuracy of our implementation is {accuracy_score(y_test, y_pred_scratch)}')
+print(f'The accuracy of sklearn implementation is {accuracy_score(y_test, y_pred_sklearn)}')
