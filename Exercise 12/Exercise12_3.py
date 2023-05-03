c62366028aa5a6849ca9a9eeb517b5ed8caf977e
Khavushka <khavushka@gmail.com>
2023-05-03 14:49:08 +0200

    Last commit

--- Exercise 12/Exercise12_3.py
+++ Exercise 12/Exercise12_3.py
@@ -1,50 +1,32 @@
 # Decision trees, naive Bayes, and k-nn classification
 # https://www.simplilearn.com/tutorials/machine-learning-tutorial/decision-tree-in-python
-import numpy as np
+# Import necessary libraries
+import pandas as pd
 from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
-# Henter Iris-dataset
+# Load Iris dataset
 iris = load_iris()
 X = iris.data
 y = iris.target
-# print(iris)
 
-# Split dataset into training
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=100)
+# Split dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+# Decision tree classification
+dt_clf = DecisionTreeClassifier(criterion='entropy', max_depth=3)
+dt_clf.fit(X_train, y_train)
+dt_pred = dt_clf.predict(X_test)
+dt_acc = accuracy_score(y_test, dt_pred)
+print("Decision tree accuracy:", dt_acc)
 
-clf_entropy = DecisionTreeClassifier(
-    criterion = "entropy",
-    random_state = 100,
-    max_depth=1,
-    # splitter = str({'random':'best'}),
-    min_samples_leaf=5,
-)
-clf_entropy.fit(X_train, y_train)
-
-# Decision Tree Classifier
-tree_clf = DecisionTreeClassifier(random_state=32)
-tree_clf.fit(X_train, y_train)
-tree_pred = tree_clf.predict(X_test)
-tree_acc = accuracy_score(y_test, tree_pred)
-print("Decision Tree:", (tree_acc)*100)
-
-# Naive Bayes Classifier
+# Naive Bayes classification
 nb_clf = GaussianNB()
 nb_clf.fit(X_train, y_train)
 nb_pred = nb_clf.predict(X_test)
-nb_acc = accuracy_score(y_test, nb_pred)
-print("Naive Bayes:", (nb_acc)*100)
-
-# k-NN Classifier
-knn_clf = KNeighborsClassifier(n_neighbors=3)
-knn_clf.fit(X_train, y_train)
-knn_pred = knn_clf.predict(X_test)
-knn_acc = accuracy_score(y_test, knn_pred)
-print("k-NN:", (knn_acc)*100)
-
+nb_acc = accuracy_score(y_test, dt_pred)
+print("Naive", nb_acc)
