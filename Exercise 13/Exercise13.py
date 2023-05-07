c0605b9f15cb07e506d79763b39558e1de918f6c
Khavushka <khavushka@gmail.com>
2023-05-07 18:27:35 +0200

    split dataset in til training og testing sets

--- Exercise 13/Exercise13.py
+++ Exercise 13/Exercise13.py
@@ -4,3 +4,14 @@ Neutral networks and support vector machines.
 - what do you observe for the apparent error and the true error?
 '''
 
+# import library
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+
+# load dataset
+iris = load_iris()
+X = iris.data
+y = iris.target
+
+# Split dataset into training og testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
