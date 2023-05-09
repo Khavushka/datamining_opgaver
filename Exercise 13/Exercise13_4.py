c25241920644fd33c5ef8402e0d3654ee5e2747f
Khavushka <khavushka@gmail.com>
2023-05-09 11:10:42 +0200

    importing SVC

---
+++ Exercise 13/Exercise13_4.py
@@ -0,0 +1,8 @@
+'''
+Neural networks and Support vector machines.
+a. choose datasets of considerably difference size: what do you observe for the training time?
+b. coose different setup for the classifiers:
+    - what do you observe for the training time?
+    - what do yoy observe for the apparent error and true error?
+'''
+from sklearn.svm import SVC
