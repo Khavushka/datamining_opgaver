cbbad3d102d300277901e0d6e4a7786f0d509247
Khavushka <khavushka@gmail.com>
2023-05-11 08:56:54 +0200

    Initial commit

--- Project 14/Exercise14.py
+++ Project 14/Exercise14.py
@@ -1,8 +1,40 @@
 import numpy as np
 import pandas as pd
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
 
+# Load the breast cancer dataset
+data = load_breast_cancer()
+X = data.data
+y = data.target
 
+# Split the dataset into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Henter dataset
-url = "http://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/wdbc.data"
-breast_cn = pd.read_csv(url, header=None)
+# Standardize the data
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+# Fit logistic regression model
+logreg = LogisticRegression()
+logreg.fit(X_train, y_train)
+y_pred_logreg = logreg.predict(X_test)
+print("Logistic Regression Accuracy:", accuracy_score(y_test, y_pred_logreg))
+
+# Fit decision tree model
+dtree = DecisionTreeClassifier()
+dtree.fit(X_train, y_train)
+y_pred_dtree = dtree.predict(X_test)
+print("Decision Tree Accuracy:", accuracy_score(y_test, y_pred_dtree))
+
+# Fit random forest model
+rf = RandomForestClassifier(n_estimators=100)
+rf.fit(X_train, y_train)
+y_pred_rf = rf.predict(X_test)
+print("Random Forest Accuracy:", accuracy_score(y_test, y_pred_rf))
