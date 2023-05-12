aa3d102758b7f3ccfc1bf84d533dcc77dd6e1626
Khavushka <khavushka@gmail.com>
2023-05-12 08:43:15 +0200

    OMG

--- Exercise 12/decision_tree.py
+++ Exercise 12/decision_tree.py
@@ -1,60 +1,102 @@
-from sklearn.datasets import load_iris
-import numpy as np
-import matplotlib.pyplot as plt
+# import pandas as pd
+# from sklearn.datasets import load_breast_cancer
+# from sklearn.tree import DecisionTreeClassifier, plot_tree
+# import matplotlib.pyplot as plt
+
+# # Load the breast cancer dataset
+# data = load_breast_cancer()
+# X = pd.DataFrame(data.data, columns=data.feature_names)
+# y = data.target
+
+# # Train a decision tree model
+# clf = DecisionTreeClassifier(random_state=42)
+# clf.fit(X, y)
+
+# # Visualize the decision tree
+# fig, ax = plt.subplots(figsize=(20, 10))
+# plot_tree(clf, ax=ax, feature_names=data.feature_names, class_names=data.target_names, filled=True)
+# plt.show()
+
+
+import pandas as pd
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.inspection import DecisionBoundaryDisplay
-from sklearn.tree import plot_tree
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
+
+# Load the breast cancer dataset
+data = load_breast_cancer()
+X = pd.DataFrame(data.data, columns=data.feature_names)
+y = data.target
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Train and evaluate a decision tree classifier
+clf_dt = DecisionTreeClassifier(random_state=42)
+clf_dt.fit(X_train, y_train)
+y_pred_dt = clf_dt.predict(X_test)
+
+accuracy_dt = accuracy_score(y_test, y_pred_dt)
+precision_dt = precision_score(y_test, y_pred_dt)
+recall_dt = recall_score(y_test, y_pred_dt)
+f1_dt = f1_score(y_test, y_pred_dt)
+cm_dt = confusion_matrix(y_test, y_pred_dt)
+
+# Train and evaluate a random forest classifier
+clf_rf = RandomForestClassifier(random_state=42)
+clf_rf.fit(X_train, y_train)
+y_pred_rf = clf_rf.predict(X_test)
+
+accuracy_rf = accuracy_score(y_test, y_pred_rf)
+precision_rf = precision_score(y_test, y_pred_rf)
+recall_rf = recall_score(y_test, y_pred_rf)
+f1_rf = f1_score(y_test, y_pred_rf)
+cm_rf = confusion_matrix(y_test, y_pred_rf)
+
+# Train and evaluate a neural network classifier
+clf_nn = MLPClassifier(random_state=42, max_iter=500)
+clf_nn.fit(X_train, y_train)
+y_pred_nn = clf_nn.predict(X_test)
+
+accuracy_nn = accuracy_score(y_test, y_pred_nn)
+precision_nn = precision_score(y_test, y_pred_nn)
+recall_nn = recall_score(y_test, y_pred_nn)
+f1_nn = f1_score(y_test, y_pred_nn)
+cm_nn = confusion_matrix(y_test, y_pred_nn)
+
+# Print the performance metrics for each classifier
+print("Decision Tree Classification Results:")
+print(f"Accuracy: {accuracy_dt:.3f}")
+print(f"Precision: {precision_dt:.3f}")
+print(f"Recall: {recall_dt:.3f}")
+print(f"F1-score: {f1_dt:.3f}")
+print(f"Confusion Matrix:\n{cm_dt}\n")
+
+print("Random Forest Classification Results:")
+print(f"Accuracy: {accuracy_rf:.3f}")
+print(f"Precision: {precision_rf:.3f}")
+print(f"Recall: {recall_rf:.3f}")
+print(f"F1-score: {f1_rf:.3f}")
+print(f"Confusion Matrix:\n{cm_rf}\n")
+
+print("Neural Network Classification Results:")
+print(f"Accuracy: {accuracy_nn:.3f}")
+print(f"Precision: {precision_nn:.3f}")
+print(f"Recall: {recall_nn:.3f}")
+print(f"F1-score: {f1_nn:.3f}")
+print(f"Confusion Matrix:\n{cm_nn}")
 
 
-iris = load_iris()
-
-# Parameters
-n_classes = 3
-plot_colors = "ryb"
-plot_step = 0.02
 
 
-for pairidx, pair in enumerate([[0, 1], [0, 2], [0, 3], [1, 2], [1, 3], [2, 3]]):
-    # We only take the two corresponding features
-    X = iris.data[:, pair]
-    y = iris.target
-
-    # Train
-    clf = DecisionTreeClassifier().fit(X, y)
-
-    # Plot the decision boundary
-    ax = plt.subplot(2, 3, pairidx + 1)
-    plt.tight_layout(h_pad=0.5, w_pad=0.5, pad=2.5)
-    DecisionBoundaryDisplay.from_estimator(
-        clf,
-        X,
-        cmap=plt.cm.RdYlBu,
-        response_method="predict",
-        ax=ax,
-        xlabel=iris.feature_names[pair[0]],
-        ylabel=iris.feature_names[pair[1]],
-    )
-
-    # Plot the training points
-    for i, color in zip(range(n_classes), plot_colors):
-        idx = np.where(y == i)
-        plt.scatter(
-            X[idx, 0],
-            X[idx, 1],
-            c=color,
-            label=iris.target_names[i],
-            cmap=plt.cm.RdYlBu,
-            edgecolor="black",
-            s=15,
-        )
-
-plt.suptitle("Decision surface of decision trees trained on pairs of features")
-plt.legend(loc="lower right", borderpad=0, handletextpad=0)
-_ = plt.axis("tight")
 
 
-plt.figure()
-clf = DecisionTreeClassifier().fit(iris.data, iris.target)
-plot_tree(clf, filled=True)
-plt.title("Decision tree trained on all the iris features")
-plt.show()
+
+# # dataset = dataset.drop(["Unnamed: 32"], axis = 1)
+# M = dataset[dataset.diagnosis == "M"]
+# B = dataset[dataset.diagnosis == "B"]
+# dataset.diagnosis = [1 if i == "M" else 0 for i in dataset.diagnosis]
+
--- Project 14/BCD.py
+++ Project 14/BCD.py
@@ -1,9 +1,9 @@
 import pandas as pd
 from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeClassifier, plot_tree
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from sklearn.neural_network import MLPClassifier
 import matplotlib.pyplot as plt
 from sklearn import tree
@@ -24,8 +24,22 @@ clf.get_params()
 clf_pred = clf.predict(X_test)
 clf.predict_proba(X_test)
 dt_acc = accuracy_score(y_test, clf_pred)
+
+# y_pred_dt = clf_dt.predict(X_test)
+acc_dt = accuracy_score(y_test, clf_pred)
+prec_dt = precision_score(y_test, clf_pred)
+rec_dt = recall_score(y_test, clf_pred)
+f1_dt = f1_score(y_test, clf_pred)
 # print("Decision tree accuracy:", dt_acc)
-print(clf_pred)
+# print(clf_pred)
+
+# Vusualization decision tree
+# fig, ax = plt.subplots(figsize=(10, 10))
+# plot_tree(clf, ax=ax, feature_names=data.feature_names, class_names=data.target_names, filled=True)
+# plt.title("Decision Tree for Breast Cancer Dataset")
+# plt.xlabel("Model")
+# plt.ylabel("Accuracy")
+# plt.show()
 
 
 # Random forest classifier
@@ -34,34 +48,64 @@ rf.fit(X_train, y_train)
 rf_pred = rf.predict(X_test)
 rf_acc = accuracy_score(y_test, rf_pred)
 # print("Random Forest accuracy:", rf_acc)
+acc_rf = accuracy_score(y_test, rf_pred)
+prec_rf = precision_score(y_test, rf_pred)
+rec_rf = recall_score(y_test, rf_pred)
+f1_rf = f1_score(y_test, rf_pred)
+
+
 
 # Neural network classifier
 nn = MLPClassifier(hidden_layer_sizes=(100,), random_state=42)
 nn.fit(X_train, y_train)
 nn_pred = nn.predict(X_test)
-nn_acc = accuracy_score(y_test, nn_pred)
-# print("Neural Network accuracy:", nn_acc)
+# nn_acc = accuracy_score(y_test, nn_pred)
+# # print("Neural Network accuracy:", nn_acc)
 
-print("Cancer data set  dimensions: {}".format(dataset.shape))
-print(dataset.head())
-results=pd.DataFrame({"Model": ["Decision Tree", "Random Forest", "Neural Network"],
-    "Accuracy": [dt_acc, rf_acc, nn_acc]})
-print(results)
+# print("Cancer data set  dimensions: {}".format(dataset.shape))
+# print(dataset.head())
+# results=pd.DataFrame({"Model": ["Decision Tree", "Random Forest", "Neural Network"],
+#     "Accuracy": [dt_acc, rf_acc, nn_acc]})
+# print(results)
+
+acc_nn = accuracy_score(y_test, nn_pred)
+prec_nn = precision_score(y_test, nn_pred)
+rec_nn = recall_score(y_test, nn_pred)
+f1_nn = f1_score(y_test, nn_pred)
+
+# Results
+# models = ["Decision Tree", "Random Forest", "Neural Network"]
+# accuracy = [dt_acc, rf_acc, nn_acc]
+# plt.bar(models, accuracy)
+# plt.title("Accuracy Scores for Breast Cancer Dataset")
+# plt.xlabel("Model")
+# plt.ylabel("Accuracy")
+# plt.show()
+
+print("Decision Tree Classifier Results:")
+print("Accuracy: {:.4f}".format(acc_dt))
+print("Precision: {:.4f}".format(prec_dt))
+print("Recall: {:.4f}".format(rec_dt))
+print("F1 Score: {:.4f}".format(f1_dt))
+print()
+print("Random Forest Classifier Results:")
+print("Accuracy: {:.4f}".format(acc_rf))
+print("Precision: {:.4f}".format(prec_rf))
+print("Recall: {:.4f}".format(rec_rf))
+print("F1 Score: {:.4f}".format(f1_rf))
+print()
+print("Neural Network Classifier Results:")
+print("Accuracy: {:.4f}".format(acc_nn))
+print("Precision: {:.4f}".format(prec_nn))
+print("Recall: {:.4f}".format(rec_nn))
+print("F1 Score: {:.4f}".format(f1_nn))
 
 
-models = ["Decision Tree", "Random Forest", "Neural Network"]
-accuracy = [dt_acc, rf_acc, nn_acc]
-plt.bar(models, accuracy)
-plt.title("Accuracy Scores for Breast Cancer Dataset")
-plt.xlabel("Model")
-plt.ylabel("Accuracy")
-plt.show()
-
-fig = plt.figure(figsize=(25,20))
-_ = tree.plot_tree(clf, 
-                    filled=True)
+# fig = plt.figure(figsize=(25,20))
+# _ = tree.plot_tree(clf, 
+#                     filled=True)
 
 
 # new data visualisation
 plt.rcParams['font.size']=10
-sns.pairplot(clf, hue='Status', palette='Blues')
+sns.pairplot(data, hue='Status', palette='Blues')
